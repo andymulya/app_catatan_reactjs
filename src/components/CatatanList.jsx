1c78dbc912b1694019c6e0ac9acf590bcfb8c37e
Andy Eka Mulya <andymulya13@gmail.com>
2023-09-19 21:43:03 +0700

    memperbaiki bug pada saat menghapus catatan

--- src/components/CatatanList.jsx
+++ src/components/CatatanList.jsx
@@ -1,14 +1,11 @@
 /* eslint-disable react/prop-types */
 import { useDispatch } from "react-redux"
-import { useState } from "react"
-import { getDetail } from "../redux/slices/catatanSlice"
+import { getDetail, removeItemById } from "../redux/slices/catatanSlice"
 import { isArchived } from "../redux/slices/catatanSlice"
 import CardItem from "./CardItem"
-import ModalDelete from "./ModalDelete"
 import ModalDetailCatatan from "./ModalDetailCatatan"
 
 const CatatanList = ({ datas }) => {
-    const [idItem, setIdItem] = useState(null)
     const dispatch = useDispatch()
 
     const handleArsip = (id) => dispatch(isArchived(id))
@@ -27,17 +24,13 @@ const CatatanList = ({ datas }) => {
                         <CardItem>
                             <CardItem.Body title={data.title} createdAt={data.createdAt} handleOnClick={() => handleDetailCatatan(data.id)} />
                             <CardItem.Actions>
-                                <div className="btn btn-xs bg-red-600 text-white" onClick={() => {
-                                    setIdItem(data.id)
-                                    document.getElementById("my_delete").showModal()
-                                }}>Hapus</div>
+                                <div className="btn btn-xs bg-red-600 text-white" onClick={() => dispatch(removeItemById(data.id))}>Hapus</div>
                                 <div className="btn btn-xs btn-outline text-cyan-600" onClick={() => handleArsip(data.id)}>Arsip</div>
                             </CardItem.Actions>
                         </CardItem>
                         {
                             <ModalDetailCatatan data={data} />
                         }
-                        <ModalDelete id={idItem} />
                     </div>
                 )
             })
